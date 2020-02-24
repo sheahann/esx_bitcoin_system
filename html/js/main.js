c233382c8975b5237768695687fb768943b1c20f
sheahann <61119848+sheahann@users.noreply.github.com>
2020-02-24 19:21:27 +0100

    Update main.js

--- html/js/main.js
+++ html/js/main.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(function() {
     $(document).on("click", "#check", function() {
         if ($(this).prop("checked") == true) {
             $("#test").fadeIn();
@@ -6,88 +6,85 @@ $(document).ready(function() {
             $("#test").fadeOut();
         }
     })
-});
 
-$(function() {
     function display(bool) {
         if (bool) {
             $("#main").slideDown();
         } else {
             $("#main").slideUp();
         }
-    }
 
-    function luaAction(type, value) {
-        $.post('http://esx_bitcoin/esx_bitcoin:jsAction', JSON.stringify({ type: type, value: value }));
-    }
-
-    window.addEventListener('message', function(event) {
-        var item = event.data;
-        if (item.enable == true) {
-            display(true)
-            $("#status span").text(item.bitcoin + 'B');
-            $("#status_id span").text(item.id);
-        } else {
-            display(false)
+        function luaAction(type, value) {
+            $.post('http://esx_bitcoin/esx_bitcoin:jsAction', JSON.stringify({ type: type, value: value }));
         }
-    })
 
-    // Custom values
+        window.addEventListener('message', function(event) {
+            var item = event.data;
+            if (item.enable == true) {
+                display(true);
+                $("#status span").text(item.bitcoin + 'B');
+                $("#status_id span").text(item.id);
+            } else {
+                display(false);
+            }
+        })
 
-    $(".customBuy").click(function() {
-        var data = $(".custom-input").val();
-        luaAction('buy', data)
-    })
+        // Custom values
 
-    $(".customSell").click(function() {
-        var data = $(".custom-input").val();
-        luaAction('sell', data)
-    })
+        $(".customBuy").click(function() {
+            var data = $(".custom-input").val();
+            luaAction('buy', data);
+        })
 
-    // Absolute values
+        $(".customSell").click(function() {
+            var data = $(".custom-input").val();
+            luaAction('sell', data);
+        })
 
-    $(".100Buy").click(function() {
-        luaAction('buy', 0.01)
-    })
+        // Absolute values
 
-    $(".100Sell").click(function() {
-        luaAction('sell', 0.01)
-    })
+        $(".Buy100").click(function() {
+            luaAction('buy', 0.01);
+        })
 
-
-
-    $(".500Buy").click(function() {
-        luaAction('buy', 0.05)
-    })
-
-    $(".500Sell").click(function() {
-        luaAction('sell', 0.05)
-    })
-
-
-
-    $(".1000Buy").click(function() {
-        luaAction('buy', 0.1)
-    })
-
-    $(".1000Sell").click(function() {
-        luaAction('sell', 0.1)
-    })
+        $(".Sell100").click(function() {
+            luaAction('sell', 0.01);
+        })
 
 
 
-    $(".2000Buy").click(function() {
-        luaAction('buy', 0.2)
-    })
+        $(".Buy500").click(function() {
+            luaAction('buy', 0.05);
+        })
 
-    $(".2000Sell").click(function() {
-        luaAction('sell', 0.2)
-    })
+        $(".Sell500").click(function() {
+            luaAction('sell', 0.05);
+        })
 
-    document.onkeyup = function(data) {
-        if (data.which == 27) {
-            $.post('http://esx_bitcoin/close', JSON.stringify({}));
-            return
-        }
+
+
+        $(".Buy1000").click(function() {
+            luaAction('buy', 0.1);
+        })
+
+        $(".Sell1000").click(function() {
+            luaAction('sell', 0.1);
+        })
+
+
+
+        $(".Buy2000").click(function() {
+            luaAction('buy', 0.2);
+        })
+
+        $(".Sell2000").click(function() {
+            luaAction('sell', 0.2);
+        })
+
+        document.onkeyup = function(data) {
+            if (data.which == 27) {
+                $.post('http://esx_bitcoin/close', JSON.stringify({}));
+            }
+        };
     };
 });

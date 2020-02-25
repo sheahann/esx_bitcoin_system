c25707ceb7568b71f761d012c95f3f50fc5f3645
sheahann <61119848+sheahann@users.noreply.github.com>
2020-02-25 20:53:24 +0100

    Update data.sql

--- data.sql
+++ data.sql
@@ -1,18 +1,14 @@
 CREATE TABLE IF NOT EXISTS `esx_bitcoin_status`(
     `Price` VARCHAR(255) NOT NULL,
-
     PRIMARY KEY (`Price`)
 );
 
-INSERT INTO `esx_bitcoin_status` (Price) VALUES
-	(100)
-;
+INSERT INTO `esx_bitcoin_status` (Price) VALUES	(100);
 
 CREATE TABLE IF NOT EXISTS `bitcoin_accounts` (
-	`identifier` VARCHAR(255) NOT NULL,
-	`ooc_name` VARCHAR(50) NOT NULL,
-	`account_id` VARCHAR(50) NOT NULL,
-	`bitcoins` INT(11) NOT NULL,
-
+	`identifier` 	VARCHAR(255) NOT NULL,
+	`ooc_name` 		VARCHAR(255) NOT NULL,
+	`account_id` 	INT NOT NULL,
+	`bitcoins` 		FLOAT NOT NULL,
 	PRIMARY KEY (`identifier`)
 );
